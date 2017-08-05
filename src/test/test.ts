4351966910878e0ad2ca76bd0d42d28b6fee7e72
Warmiq <todor.o.todorov93@gmail.com>
2017-08-05 16:29:37 +0100

    setup test folder struct

---
+++ src/test/test.ts
@@ -0,0 +1,12 @@
+import * as chai from "chai";
+
+
+describe("Test 1", () => {
+
+    it("should always work", () => {
+
+        const banana = 1;
+
+        chai.expect(1).to.be.deep.equal(banana);
+    })
+})
